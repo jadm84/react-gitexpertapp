736ea8d2d280a56ee727a4c1708d91f01b5b7dea
Juan Antonio Domínguez Martín <jadominguez@isotrol.com>
2020-12-12 18:33:18 +0100

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import {React, useState} from 'react';
+import AddCategory from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    const [categories, setCategory] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr/>
+
+            <AddCategory setCategory={setCategory}/>
+
+            <ol>
+                {
+                    categories.map(category => (
+                        <GifGrid 
+                            key={category} 
+                            category={category}
+                        />
+                    ))
+                }
+            </ol>
+        </>
+    );
+};
+
+export default GifExpertApp;
